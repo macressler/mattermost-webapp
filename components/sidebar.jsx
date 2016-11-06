8146c8a70f3ef535561137598fd68a7e7e2704f7
kaifaust <kaifaust@gmail.com>
2016-11-05 17:02:37 -0700

    All White labeling (squashed)
    Redesign mobile login
    
    Hide footer on all screens
    
    Change the color of the error bar
    
    Config changes for app name. Change hardcoded defaults. Add svg icon. Use UChat favicons.
    
    Change hardcoded defaults
    
    Add svg icon
    
    Use UChat favicons
    
    Fix case for uChat
    
    Add active favicon
    
    TSDEV-91 Replace brand name in root html and manifest.json
    
    TSDEV-52 Replace copyright colophon, changes in AboutBuildModal
    
    TSDEV-107 Replace logo icon used in system posts in RHS
    
    [TSDEV-51] display user fullname instead of team name on top-left
    
    TSDEV-119 remove mattermost icon from file upload overlay
    
    TSDEV-80 Whitelabel mobile browser intercept screens #43
    
    styling fixes for mobile intercept page
    
    add logo locally
    
    TSDEV-79 UCHAT-46 Whitelabel email templates #48
    
    UCHAT-131 Whitelabel welcome email template
    
    UCHAT-182 Add missing preposition to mention email subject
    
    UCHAT-78 Configure app url scheme/intent for android app
    
    UCHAT-200 Whitelabel alert message for websocket disconnection
    
    UCHAT-125 Clarify download button text in mobile browser intercept screens
    
    UCHAT-238 replace favicons with uchat icons
    
    UCHAT-186 set a min-height for the sidebar menu header
    
    UCHAT-335 update channel and dm icons
    
    UCHAT-388 In dark theme active menu item text is too muted
    
    UCHAT-54 rename private group to private channel
    
    Merge new styles that came from MM-release-3.8-shift and that add value to uChat styles. Fix gray uchat icon in about modal.
    
    Create paralel Style files to avoid conflicts with MM style changes.( Squash to white label)
    
    Removed top padding for buttons to appear correctly in smaller screen devices
    
    change header LHS header to black and remove modal header button border (#59)
    
    UCHAT-1781 Clean up styles and merge update to MM release-3.10 styles
    
    TSDEV-111 fix gray uchat icon in about modal
    
    TSDEV-143 fix aspect ratio issue with rectangular profile images
    
    UCHAT-232 add error message to root.html that is displayed during network error
    
    UCHAT-1893 Clean up styles and merge updates from MM release-4.0 styles
    
    Styles updates 4.2 (whitelabel)

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -555,7 +555,10 @@ export default class Sidebar extends React.Component {
 
         let closeButton = null;
         let removeTooltip = (
-            <Tooltip id='remove-dm-tooltip'>
+            <Tooltip
+                className='hidden-xs'
+                id='remove-dm-tooltip'
+            >
                 <FormattedMessage
                     id='sidebar.removeList'
                     defaultMessage='Remove from list'
@@ -595,6 +598,7 @@ export default class Sidebar extends React.Component {
         let tutorialTip = null;
         if (this.state.showTutorialTip && channel.name === Constants.DEFAULT_CHANNEL) {
             tutorialTip = this.createTutorialTip();
+            this.openLeftSidebar();
         }
 
         let link = '';
@@ -722,7 +726,10 @@ export default class Sidebar extends React.Component {
         }
 
         const createChannelTootlip = (
-            <Tooltip id='new-channel-tooltip' >
+            <Tooltip
+                className='hidden-xs'
+                id='new-channel-tooltip'
+            >
                 <FormattedMessage
                     id='sidebar.createChannel'
                     defaultMessage='Create new public channel'
@@ -730,7 +737,10 @@ export default class Sidebar extends React.Component {
             </Tooltip>
         );
         const createGroupTootlip = (
-            <Tooltip id='new-group-tooltip'>
+            <Tooltip
+                className='hidden-xs'
+                id='new-group-tooltip'
+            >
                 <FormattedMessage
                     id='sidebar.createGroup'
                     defaultMessage='Create new private channel'
