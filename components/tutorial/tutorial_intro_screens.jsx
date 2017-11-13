mattermost/mattermost

UCHAT-143 Remove tutorial reference to search + archiving

webapp/components/tutorial/tutorial_intro_screens.jsx
@@ -136,8 +136,7 @@ export default class TutorialIntroScreens extends React.Component {
                 <FormattedHTMLMessage
                     id='tutorial_intro.screenTwo'
                     defaultMessage='<h3>How uChat works:</h3>
-                    <p>Communication happens in public discussion channels, private groups and direct messages.</p>
-                    <p>Everything is archived and searchable from any web-enabled desktop, laptop or phone.</p>'
+                    <p>Communication happens in public discussion channels, private groups and direct messages.</p>'
                 />
                 {appDownloadLink}
                 {appDownloadImage}

macressler/mattermost-webapp

components/tutorial/tutorial_intro_screens.jsx
@@ -167,8 +167,7 @@ export default class TutorialIntroScreens extends React.Component {
                 <FormattedHTMLMessage
                     id='tutorial_intro.screenTwo'
                     defaultMessage='<h3>How uChat works:</h3>
-                    <p>Communication happens in public discussion channels, private channels and direct messages.</p>
-                    <p>Everything is archived and searchable from any web-enabled desktop, laptop or phone.</p>'
+                    <p>Communication happens in public discussion channels, private channels and direct messages.</p>'
                 />
                 {appDownloadLink}
                 {appDownloadImage}