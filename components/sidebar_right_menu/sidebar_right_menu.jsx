2678298395343595d759f976eca1710297318b71
Chris Duarte <csduarte@users.noreply.github.com>
2018-01-05 09:25:58 -0800

    Allow default team to be configured (#334)
    * Allow default team to be configured
    
    * Rename DefaultTeamName to ExperimentalPrimaryTeam

--- components/sidebar_right_menu/sidebar_right_menu.jsx
+++ components/sidebar_right_menu/sidebar_right_menu.jsx
@@ -239,7 +239,7 @@ export default class SidebarRightMenu extends React.Component {
                 }
             }
 
-            if (moreTeams) {
+            if (moreTeams && !global.mm_config.ExperimentalPrimaryTeam) {
                 joinAnotherTeamLink = (
                     <li key='joinTeam_li'>
                         <Link to='/select_team'>
@@ -285,24 +285,27 @@ export default class SidebarRightMenu extends React.Component {
             </li>
         );
 
-        const leaveTeam = (
-            <li key='leaveTeam_li'>
-                <a
-                    id='leaveTeam'
-                    href='#'
-                    onClick={GlobalActions.showLeaveTeamModal}
-                >
-                    <span
-                        className='icon'
-                        dangerouslySetInnerHTML={{__html: Constants.LEAVE_TEAM_SVG}}
-                    />
-                    <FormattedMessage
-                        id='navbar_dropdown.leave'
-                        defaultMessage='Leave Team'
-                    />
-                </a>
-            </li>
-        );
+        let leaveTeam = '';
+        if (!global.mm_config.ExperimentalPrimaryTeam) {
+            leaveTeam = (
+                <li key='leaveTeam_li'>
+                    <a
+                        id='leaveTeam'
+                        href='#'
+                        onClick={GlobalActions.showLeaveTeamModal}
+                    >
+                        <span
+                            className='icon'
+                            dangerouslySetInnerHTML={{__html: Constants.LEAVE_TEAM_SVG}}
+                        />
+                        <FormattedMessage
+                            id='navbar_dropdown.leave'
+                            defaultMessage='Leave Team'
+                        />
+                    </a>
+                </li>
+            );
+        }
 
         if (isAdmin) {
             teamSettingsLink = (
